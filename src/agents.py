30dc464ed6dfe22e07891ebd470b711f218488b5
Luan Ciribelli <44177584+LuanCiribelli@users.noreply.github.com>
2023-10-26 16:54:40 -0300

    Adicionando estudantes com base no horario

--- src/agents.py
+++ src/agents.py
@@ -1,13 +1,12 @@
 from mesa import Agent
-from utilities import manhattan_distance, MovementUtils
 from constants import *
 from mapa.mapa_RU import CellType
- 
-
-
+from constants import *
 
 
 class StudentAgent(Agent):
+    STATES = ["SEARCHING_TRAY", "GOING_TO_JUICE", "GOING_TO_SPICES", "GOING_TO_DESSERT", "GOING_TO_TABLE", "EXITING"]
+    
     def __init__(self, unique_id, model, x, y):
         super().__init__(unique_id, model)
         self.pos = (x, y)
@@ -15,23 +14,9 @@ class StudentAgent(Agent):
         self.waiting_time = 0
         self.blocked_steps = 0
         self.visited_groups = set()
-
-    def reached_goal(self, goal):
-        if self.state == "SEARCHING_TRAY":
-            tray_cell_contents = self.model.grid.get_cell_list_contents([goal])
-            for content in tray_cell_contents:
-                if content.type == "Bandeja" and content.portions > 0:
-                    content.portions -= 1
-            self.state = "EXITING"
-        elif self.state == "EXITING":
-            current_cell_contents = self.model.grid.get_cell_list_contents([self.pos])
-            if any(content.type == "S" for content in current_cell_contents if hasattr(content, 'type')):
-                self.model.grid.remove_agent(self)
-                self.model.schedule.remove(self)
-
-
+    
     def move_towards(self, goal):
-        valid_moves = self.model.movement_utils.valid_moves(self, goal, "StudentAgent")
+        valid_moves = self.model.movement_utils.valid_moves(self, goal)
         if valid_moves:
             best_step = min(valid_moves, key=lambda step: manhattan_distance(step, goal))
             self.model.grid.move_agent(self, best_step)
@@ -40,87 +25,81 @@ class StudentAgent(Agent):
             self.blocked_steps += 1  # Increment blocked_steps if no valid move
 
     def determine_goal(self):
-        """ Determines the current goal of the student based on its state. """
         if self.state == "SEARCHING_TRAY":
-            return self.model.pathfinding.nearest_tray(self)
-        elif self.state == "WAITING_AT_TRAY":
-            return self.pos
+            tray_coords = self.model.locations_cache['trays']
+            if not tray_coords:
+                return self.pos  # If no trays, remain in position
+            return self.model.movement_utils.nearest(self, CellType.TRAY)
+        elif self.state == "GOING_TO_JUICE":
+            nearest_juice = self.model.movement_utils.nearest(self, CellType.JUICE)
+            return self.get_adjacent_empty_cell(nearest_juice)
+        elif self.state == "GOING_TO_SPICES":
+            spice_coords = self.model.locations_cache['spices']
+            return self.model.movement_utils.nearest(self, CellType.SPICES)
+        elif self.state == "GOING_TO_DESSERT":
+            dessert_coords = self.model.locations_cache['desserts']
+            return self.model.movement_utils.nearest(self, CellType.DESSERT)
+        elif self.state == "GOING_TO_TABLE":
+            table_coords = self.model.locations_cache['tables']
+            return self.random.choice(table_coords)
         elif self.state == "EXITING":
-            return self.model.pathfinding.nearest_exit(self)
+            return self.model.movement_utils.nearest_exit(self)
+        else:
+            return self.pos  # Default case if an unknown state
+
+    def get_adjacent_empty_cell(self, target_pos):
+        x, y = target_pos
+        possible_adjacent_cells = [(x+dx, y+dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                                if 0 <= x+dx < self.model.width and 0 <= y+dy < self.model.height]
+        for cell in possible_adjacent_cells:
+            if not self.model.grid.get_cell_list_contents([cell]):
+                return cell
+        return self.pos  # Default: return the agent's own position if no adjacent empty cell found
+
+
 
     def reached_goal(self, goal):
-        if self.state == "SEARCHING_TRAY":
-            tray_cell_contents = self.model.grid.get_cell_list_contents([goal])
-            for content in tray_cell_contents:
-                if hasattr(content, 'type') and content.type == "Bandeja" and content.portions > 0: 
-                    content.portions -= 1
-                    self.visited_groups.add(goal)
-            self.state = "EXITING"
-        elif self.state == "WAITING_AT_TRAY":
-            self.waiting_time += 1
-            if self.waiting_time >= WAITING_TIME_THRESHOLD:
-                self.state = "EXITING"
-        elif self.state == "EXITING":
-            current_cell_contents = self.model.grid.get_cell_list_contents([self.pos])
-            if any(content.type == "S" for content in current_cell_contents if hasattr(content, 'type')):
-                self.model.grid.remove_agent(self)
-                self.model.schedule.remove(self)
+        transitions = {
+            "SEARCHING_TRAY": "GOING_TO_JUICE",
+            "GOING_TO_JUICE": "GOING_TO_DESSERT",
+            "GOING_TO_DESSERT": "GOING_TO_SPICES",
+            "GOING_TO_SPICES": "GOING_TO_TABLE",
+            "GOING_TO_TABLE": "EXITING"
+        }
+        self.state = transitions.get(self.state, "EXITING")
+
+        if self.state == "EXITING":
+            # Remove agent from the grid and schedule when exiting
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+
 
     def step(self):
-        if self.state != "WAITING_AT_TRAY":
-            goal = self.determine_goal()
-            if self.is_at_goal(goal):
-                self.reached_goal(goal)
-            else:
-                self.move_towards(goal)
+        goal = self.determine_goal()
+        
+        # Get the content (cell type) at the goal position
+        
+        goal_content = self.model.grid.get_cell_list_contents([goal])
+        if goal_content:
+            target_cell_type = next((content.type for content in goal_content if hasattr(content, 'type')), None)
         else:
-            self.reached_goal(self.pos)
+            target_cell_type = None
 
+        print(f"Student at position {self.pos}. In state {self.state} trying to go to position {goal} to get to the cell type {target_cell_type}")
+        
+        if self.is_at_goal(goal):
+            self.reached_goal(goal)
+        else:
+            self.move_towards(goal)
+
+            
     def is_at_goal(self, goal):
         return self.pos == goal
 
+    def is_valid_position(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.height
 
-
-
-
-
-class Pathfinding:
-    def __init__(self, model, movement_utils):
-        self.model = model
-        self.movement_utils = movement_utils
-
-    def students_around(self, tray):
-        x, y = tray
-        locations = [(x+i, y+j) for i in [-1, 0, 1] for j in [-1, 0, 1]]
-        return sum(1 for loc in locations if any(isinstance(content, StudentAgent) for content in self.model.grid.get_cell_list_contents([loc])))
-
-    def nearest(self, agent, target_type):
-        x, y = agent.pos
-        target_coords = [(i, j) for i, row in enumerate(self.model.external_grid)
-                         for j, cell in enumerate(row) if cell == target_type]
-        return min(target_coords, key=lambda pos: manhattan_distance((x, y), pos))
-
-    def nearest_exit(self, agent):
-        return self.nearest(agent, CellType.EXIT)
-
-    def nearest_tray(self, agent):
-        # First, get all tray coordinates
-        tray_coords = [(i, j) for i, row in enumerate(self.model.external_grid)
-                    for j, cell in enumerate(row) if cell == CellType.TRAY]
-        
-        # Filter out tray groups already visited by this student
-        unvisited_tray_coords = [coord for coord in tray_coords if coord not in agent.visited_groups]
-        
-        if not unvisited_tray_coords:  # All tray groups visited
-            return None
-        
-        # Find the nearest unvisited tray
-        nearest_unvisited_tray = min(unvisited_tray_coords, key=lambda pos: manhattan_distance(agent.pos, pos))
-        
-        return nearest_unvisited_tray
-
-
-
+    
 class StaticAgent(Agent):
     def __init__(self, unique_id, model, x, y, agent_type, content=None):
         super().__init__(unique_id, model)
@@ -137,4 +116,75 @@ class StaticAgent(Agent):
                 self.portions = DEFAULT_TRAY_PORTIONS
         else:
             self.portions = None
+
+AGENT_CLASSES = {
+    "StudentAgent": StudentAgent,
+    "StaticAgent": StaticAgent,
+}
+
+
+def manhattan_distance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+
+class MovementUtils:
+    def __init__(self, model): 
+        self.model = model
+
+    @staticmethod
+    def valid_moves(agent, goal):
+        x, y = agent.pos
+        possible_steps = [(x+dx, y+dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)] 
+                        if 0 <= x+dx < agent.model.width and 0 <= y+dy < agent.model.height
+                        and not agent.model.grid.get_cell_list_contents([(x+dx, y+dy)])]
+    
+
+        return possible_steps
+
+    def is_valid_cell(self, cell_contents):
+        return all(content.type != "Parede" for content in cell_contents if hasattr(content, 'type')) and not any(isinstance(content, StudentAgent) for content in cell_contents)
+
+    def students_around(self, tray):
+        x, y = tray
+        
+        # Assuming the grid is of size grid_width x grid_height
+        grid_width, grid_height = self.model.grid.width, self.model.grid.height
+
+        # Calculate all neighboring locations
+        locations = [(x+i, y+j) for i in [-1, 0, 1] for j in [-1, 0, 1]]
+
+        # Filter locations to only those inside the grid
+        valid_locations = [(x, y) for x, y in locations if 0 <= x < grid_width and 0 <= y < grid_height]
+
+        # Count the number of students in the valid locations
+        
+        return sum(1 for loc in valid_locations if any(isinstance(content, StudentAgent) for content in self.model.grid.get_cell_list_contents([loc])))
+
+
+    def nearest(self, agent, target_type):
+            x, y = agent.pos
+            target_coords = [(i, j) for i, row in enumerate(self.model.external_grid)
+                            for j, cell in enumerate(row) if cell == target_type]
+
+            if target_type == CellType.TABLE:
+                # Sort tables by how many student agents are around them.
+                target_coords = sorted(target_coords, key=lambda pos: self.students_around(pos))
+
+            return min(target_coords, key=lambda pos: manhattan_distance((x, y), pos))
+
+
+    def nearest_exit(self, agent):
+        return self.nearest(agent, CellType.EXIT)
+
+    def nearest_tray(self, agent):
+        tray_coords = [(i, j) for i, row in enumerate(self.model.external_grid)
+                    for j, cell in enumerate(row) if cell == CellType.TRAY]
+
+        # Sort trays by the number of portions left (in ascending order).
+        sorted_tray_coords = sorted(tray_coords, key=lambda pos: self.model.grid.get_cell_list_contents([pos])[0].portions)
+
+        if not sorted_tray_coords:
+            return None
+
+        return sorted_tray_coords[0]  # return the tray with the most portions left.
 
--- src/model.py
+++ src/model.py
@@ -1,45 +1,51 @@
-
-
-
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
-from mesa.visualization.modules import  TextElement
+from mesa.visualization.modules import TextElement
 from mesa.datacollection import DataCollector
 
-from mapa.mapa_RU import  CellType
+import pandas as pd
+from datetime import datetime
+
+from mapa.mapa_RU import CellType
 from constants import *
-from agents import StudentAgent, Pathfinding, StaticAgent
-from utilities import MovementUtils
+from agents import StudentAgent, StaticAgent, MovementUtils
 
 
+DATAFRAME = pd.read_csv('../logentrada.csv')
 
-# 3. Modelo e Visualização
+# Convert 'Entrada' to datetime
+DATAFRAME['Entrada'] = pd.to_datetime(DATAFRAME['Entrada'])
+
+# Filter for a specific day and "Almoço"
+desired_date = '2023-01-05'
+filtered_df = DATAFRAME[(DATAFRAME['Entrada'].dt.date == datetime.strptime(desired_date, '%Y-%m-%d').date()) & 
+                        (DATAFRAME['Refeicao'] == 'Almoco')]
+
+# Compute total seconds from the start of the day
+filtered_df['seconds_from_start'] = filtered_df['Entrada'].dt.hour * 3600 + filtered_df['Entrada'].dt.minute * 60 + filtered_df['Entrada'].dt.second
+filtered_df = filtered_df.sort_values(by='seconds_from_start')
+
 
 class ModelText(TextElement):
     def __init__(self):
         pass
-    
+
     def render(self, model):
         student_agents = [agent for agent in model.schedule.agents if isinstance(agent, StudentAgent)]
         avg_waiting_time = sum(agent.waiting_time for agent in student_agents) / len(student_agents) if student_agents else 0
-        return f"Passo atual: {model.time} | Tempo médio de espera: {avg_waiting_time}"
+        return f"Current Step: {model.time} | Average Waiting Time: {avg_waiting_time}"
 
-
-
-
-# Modelo que representa o ambiente do restaurante
 class RestaurantModel(Model):
-
     AGENT_TYPE_MAPPING = {
-        CellType.TURNSTILE: 'Catraca',
-        CellType.WALL: 'Parede',
-        CellType.TRAY: 'Bandeja',
-        CellType.EXIT: 'Saida',
-        CellType.JUICE: 'Suco',
-        CellType.SPICES: 'Tempero',
-        CellType.DESSERT: 'Sobremesa',
-        CellType.TABLE: 'Mesa',
+        CellType.TURNSTILE: 'Turnstile',
+        CellType.WALL: 'Wall',
+        CellType.TRAY: 'Tray',
+        CellType.EXIT: 'Exit',
+        CellType.JUICE: 'Juice',
+        CellType.SPICES: 'Spices',
+        CellType.DESSERT: 'Dessert',
+        CellType.TABLE: 'Table',
     }
 
     def __init__(self, external_grid):
@@ -47,7 +53,6 @@ class RestaurantModel(Model):
         self.width = len(external_grid[0])
         self.external_grid = external_grid
         self.movement_utils = MovementUtils(self)
-        self.pathfinding = Pathfinding(self, self.movement_utils)
         self.steps_since_last_student = 0
         self.datacollector = DataCollector({
             "Average_Waiting_Time": lambda m: sum([agent.waiting_time for agent in m.schedule.agents if isinstance(agent, StudentAgent)]) / (len([agent for agent in m.schedule.agents if isinstance(agent, StudentAgent)]) or 1)
@@ -55,25 +60,37 @@ class RestaurantModel(Model):
 
         self.grid = MultiGrid(self.width, self.height, True)
         self.schedule = RandomActivation(self)
-        self.time = 0
+        self.start_hour = 11  # Starting from 11:00:00
+        self.time = self.start_hour * 3600  # Convert the starting hour to seconds
         self.error_message = None
         self.next_id = 0
+        self.filtered_df = filtered_df
+        self.locations_cache = {
+            'trays': self.find_cell_positions(CellType.TRAY),
+            'juices': self.find_cell_positions(CellType.JUICE),
+            'spices': self.find_cell_positions(CellType.SPICES),
+            'desserts': self.find_cell_positions(CellType.DESSERT),
+            'tables': self.find_cell_positions(CellType.TABLE),
+            'exits': self.find_cell_positions(CellType.EXIT)
+        }
 
         for y, row in enumerate(external_grid):
             for x, cell_value in enumerate(row):
                 if cell_value == CellType.STUDENT:
                     student = StudentAgent((x, y), self, x, y)
                     self.grid.place_agent(student, (x, y))
-                    self.schedule.add(student)             
+                    self.schedule.add(student)
                 elif cell_value in self.AGENT_TYPE_MAPPING:
                     agent_type = self.AGENT_TYPE_MAPPING[cell_value]
                     self.grid.place_agent(StaticAgent((x, y), self, x, y, agent_type), (x, y))
 
     def step(self):
         """Defines the action taken in each time step of the simulation."""
-        print("Método step foi chamado!")
-        
-        # Se houver uma mensagem de erro, interrompa a simulação.
+        current_time = self.get_human_readable_time()
+        print(f"Step method called! Current time: {current_time}")
+
+
+        # If there is an error message, stop the simulation.
         if self.error_message:
             print(self.error_message)
             return
@@ -81,48 +98,71 @@ class RestaurantModel(Model):
         self.time += 1
         self.schedule.step()
 
-        # No primeiro step ou a cada 8 steps, adicione um estudante.
-        if self.time == 1 or self.time % 8 == 0:
-            self.add_new_student()
+        # Add a new student on the first step or every 8 steps.
+        matching_rows = self.filtered_df[self.filtered_df['seconds_from_start'] == self.time]
+        for _, row in matching_rows.iterrows():
+         self.add_new_student(catraca_id=row['IDCatraca'])
 
         self.datacollector.collect(self)
-    
-        # Verifique se todos os estudantes estão bloqueados.
+        '''
+        # Check if all students are blocked.
         all_students_blocked = all([student.blocked_steps >= MAX_BLOCKED_STEPS for student in self.schedule.agents if isinstance(student, StudentAgent)])
         if all_students_blocked:
-            self.error_message = "Erro, todos os estudantes presos, modelo parado"
+            self.error_message = "Error, all students blocked, model stopped"
             print(self.error_message)
             return
-
-        # Se não houver mais nenhum StudentAgent, pare a simulação.
+    
+        # If there are no more StudentAgents, stop the simulation.
         if not any(isinstance(agent, StudentAgent) for agent in self.schedule.agents):
             self.running = False
-
+        '''
     def get_next_id(self):
         self.next_id += 1
         return self.next_id
 
-    def add_new_student(self):
-            print("Tentando adicionar novo estudante...")
-            turnstile_coords = [(0, 0), (0, 1),(61,0),(61,1)]  # Coordenadas das catracas
-            entry_coords = [(1, 0), (1, 1),(60,0),(60,1) ]  # Coordenadas à direita das catracas
-          
-            # Escolhe uma entrada aleatória das disponíveis
+    def find_cell_positions(self, cell_type):
+        return [(i, j) for i, row in enumerate(self.external_grid)
+                for j, cell in enumerate(row) if cell == cell_type]
+
+    def update_cache(self, cell_type):
+        """To be called whenever there's a change in a specific type of cell."""
+        self.locations_cache[cell_type] = self.find_cell_positions(cell_type)
+
+
+    def get_human_readable_time(self):
+        hours = self.time // 3600
+        minutes = (self.time % 3600) // 60
+        seconds = self.time % 60
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+    def add_new_student(self, catraca_id):
+
+
+        catraca_mapping = {1: (1, 2), 2: (1, 5), 3: (82, 5), 4: (82, 2)}
+        entry_coords = [(1, 2), (1, 5), (82, 5), (82, 2)]
+
+        chosen_entry = catraca_mapping.get(catraca_id)
+        if not chosen_entry:
+            print(f"Warning: Catraca ID {catraca_id} not found in mapping. Choosing random entry.")
             chosen_entry = self.random.choice(entry_coords)
 
-            # Verifica se a entrada escolhida está vazia
-            if not self.grid.get_cell_list_contents([chosen_entry]):
-                print(f"Adicionando estudante em {chosen_entry}...")
-                student_id = self.get_next_id()
-                student = StudentAgent(student_id, self, *chosen_entry)
-                self.grid.place_agent(student, chosen_entry)
-                self.schedule.add(student)
 
+        # Check if the chosen entry is empty
+        if not self.grid.get_cell_list_contents([chosen_entry]):
+            print(f"Adding student at {chosen_entry}...")
+            student_id = self.get_next_id()
+            student = StudentAgent(student_id, self, *chosen_entry)
+            self.grid.place_agent(student, chosen_entry)
+            self.schedule.add(student)
+        
+        
+        print(f"Trying to add a new student at {chosen_entry}")
 
 def agent_portrayal(agent):
     """Defines the visual portrayal of agents in the simulation."""
     if isinstance(agent, StudentAgent):
-        if agent.waiting_time > WAITING_TIME_THRESHOLD:  
+        if agent.waiting_time > WAITING_TIME_THRESHOLD:
             color = "orange"
         elif agent.model.error_message:
             color = "red"
@@ -137,25 +177,25 @@ def agent_portrayal(agent):
         }
     else:  # This covers all other types of agents, including trays.
         shape_colors = {
-            "Catraca": "gray",
-            "Parede": "black",
-            "Bandeja": "green",  # Default color, but can be overridden below
-            "Saida": "red",
-            "Suco": "blue",
-            "Tempero": "purple",
-            "Sobremesa": "gold",
-            "Mesa": "#766c6a",
+            "Turnstile": "gray",
+            "Wall": "black",
+            "Tray": "green",  # Default color, but can be overridden below
+            "Exit": "red",
+            "Juice": "blue",
+            "Spices": "purple",
+            "Dessert": "gold",
+            "Table": "#766c6a",
         }
 
         # Override the color for trays based on content and portions.
-        if agent.type == "Bandeja":
+        if agent.type == "Tray":
             if agent.content == "vegan":
-                shape_colors["Bandeja"] = "lightgreen"
+                shape_colors["Tray"] = "lightgreen"
             elif agent.content == "meat":
-                shape_colors["Bandeja"] = "brown"
+                shape_colors["Tray"] = "brown"
             if agent.portions == 0:
-                shape_colors["Bandeja"] = "red"
-
+                shape_colors["Tray"] = "red"
+ 
         return {
             "Shape": "rect",
             "Color": shape_colors.get(agent.type, "white"),
@@ -164,9 +204,3 @@ def agent_portrayal(agent):
             "w": 1,
             "h": 1
         }
-
-
-
-
-
-
